91d72536aa2253324ff8c0b855f6f4c37d29f8eb
SushilMall171096 <sushilmall96@gmail.com>
2018-10-26 20:14:12 +0530

    Hyper Cube

---
+++ Hyper Cube.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Hyper_Cube
+{
+	
+public static void main(String[] args) {
+   Scanner sc=new Scanner(System.in);
+   int n=sc.nextInt();
+   System.out.println((int)Math.pow(2,n));
+}
+}
